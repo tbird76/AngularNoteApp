c1d8310220f9c1fa7727c01fccae9286522af0a7
Ty Bird <t.bird7976@gmail.com>
2023-08-09 07:07:19 -0700

    added logged in user to header. added logout button

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { BackendService } from 'src/app/service/backend.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  constructor(private router: Router, private server: BackendService){}
+
+  ngOnInit() {
+    this.loggedInUser = this.server.getLoggedInUser();
+  }
+  
+  loggedInUser!: User;
+  
+
+  logout(){
+    this.router.navigate(['login']);
+  }
 
 }
